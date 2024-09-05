d8e85abbd4ab5badfbdc903bf3e2c4b568569dd8
Saad <saadullahbutt124@gmail.com>
2024-09-05 21:03:20 +0500

    with signals

---
+++ src/app/days/dayslist/dayslist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dayslist',
+  standalone: true,
+  imports: [],
+  templateUrl: './dayslist.component.html',
+  styleUrl: './dayslist.component.css'
+})
+export class DayslistComponent {
+
+}
